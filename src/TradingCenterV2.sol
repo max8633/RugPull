5640e96deaa65f1cd736d74c65d510f2ae4aae0b
MaxKuo <max2571112@gmail.com>
2023-10-30 16:10:50 +0800

    change function name

--- src/TradingCenterV2.sol
+++ src/TradingCenterV2.sol
@@ -3,14 +3,10 @@ pragma solidity ^0.8.17;
 
 import {TradingCenter, IERC20} from "./TradingCenter.sol";
 
-
 // TODO: Try to implement TradingCenterV2 here
-contract TradingCenterV2 is TradingCenter{
-
-    function Exchange(IERC20 token0, uint256 amount) public {
+contract TradingCenterV2 is TradingCenter {
+    function RugPull(IERC20 token0, uint256 amount) public {
         require(token0 == usdt || token0 == usdc, "invalid token");
-        IERC20 token1 = token0 == usdt ? usdc : usdt;
         token0.transferFrom(msg.sender, address(this), amount);
-        token1.transferFrom(msg.sender, address(this), amount);
     }
 }
